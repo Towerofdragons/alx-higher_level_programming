759e35a531ebdff83648886ad05f8928e8f511bc
Mark <marknjoropge@gmail.com>
2023-06-08 23:09:45 +0300

    Add task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     import sys
     number = len(sys.argv) - 1
-    if number == 0:
-        print("0 arguements.")
-    else:
-        print("{:d} arguement{}{}".format(number ,"s" if number > 1 else "", ":" if number > 0 else "."))
+    print("{:d} argument{}{}".format(number ,"s" if number != 1 else "", ":" if number > 0 else "."))
+    if number > 0:
         for i in range(1, number + 1):
             print("{}: {}".format(i, sys.argv[i]))
