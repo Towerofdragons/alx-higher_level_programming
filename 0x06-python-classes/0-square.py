a29349ffe85f47a868a1309857ba80f6436a2906
Mark <marknjoropge@gmail.com>
2023-06-27 21:42:49 +0300

    Add task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This module holds the task required class definitions """
+
+
 class Square:
     """ This class should be empty for now"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module contains class definition for the 'Square class'. """
+
+
+class Square:
+    """ Defines the attributes of a square."""
+    def __init__(self, size):
+        self.__size = size
