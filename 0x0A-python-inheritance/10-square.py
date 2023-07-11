652833ef2f7bb48b5838144067d6b9ff160231eb
Mark <marknjoropge@gmail.com>
2023-07-11 05:27:51 +0300

    Add task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module defines Class Square inheriting from Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    """
+    Class definition for squares inheriting from Rectangle
+    """
+    
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
