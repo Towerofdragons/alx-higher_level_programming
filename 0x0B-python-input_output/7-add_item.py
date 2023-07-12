26cd7743553fc82f04daf969b4c3c284dc3a1d8f
Mark <marknjoropge@gmail.com>
2023-07-13 02:33:04 +0300

    Add task 5 and 6

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module defines function 'json_modify'
+"""
+
+
+if __name__ == "__main__":
+
+
+    import json
+    import sys
+
+
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file= __import__("6-load_from_json_file").load_from_json_file
+
+
+    filename = "add_item.json"
+    try:
+        obj = load_from_json_file(filename)
+    except Exception:
+        obj = []
+        save_to_json_file(obj, filename)
+    print(obj)
+    obj.extend(sys.argv[1:])
+    print(obj)
+    save_to_json_file(obj, filename)
+
+
+
