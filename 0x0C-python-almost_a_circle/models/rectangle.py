518762005729a0ca919bfb3eda2c072e0d467bf9
Mark <marknjoropge@gmail.com>
2023-07-17 22:28:43 +0300

    Modified ID test cases

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,8 @@ Module defines Rectangle class.
 from modules.base import Base
 
 class Rectangle(Base):
+
+    """Represent a rectangle."""
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
