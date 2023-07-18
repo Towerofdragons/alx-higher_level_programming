777421b994abed7d9c5edd096e4e8a5f05743f41
Mark <marknjoropge@gmail.com>
2023-07-18 17:48:16 +0300

    Rewrote Rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,4 +64,71 @@ class Rectangle(Base):
             raise ValueError("y must be > 0")
         self.__y = value
 
+    def area(self):
+        """
+        Returns area value of the rectangle instance.
+        """
+        return self.height * self.width
 
+    def display(self):
+        """
+        Prints the rectangle to dimensions (height, width).
+        Observes an offset to dimensions (x, y).
+        """
+
+        if self.y:
+            for y in range(self.y):
+                print()
+
+        for h in range(self.height):
+            if self.x:
+                for x in range(self.x):
+                    print(" ", end='')
+
+            for w in range(self.width):
+                print("#", end='')
+            print()
+
+
+    def update(self, *args, **kwargs):
+
+        if args and len(args) > 0:
+
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        return {
+                'x': self.x,
+                'y': self.y,
+                'id': self.id,
+                'height': self.height,
+                'width': self.width
+                }
+
+    def __str__(self):
+        return(f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
