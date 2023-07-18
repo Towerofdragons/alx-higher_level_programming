8f200039f70569b6704527df179f5683b4e577a6
Mark <marknjoropge@gmail.com>
2023-07-18 18:58:19 +0300

    Fixed TypeError error messages for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,7 +48,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError ("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
 
@@ -61,7 +61,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError ("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
 
