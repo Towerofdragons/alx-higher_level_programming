ffc39e3c1b7ce11e810ca0f716e4129fd2467571
Mark <marknjoropge@gmail.com>
2023-07-21 23:00:54 +0300

    Add test for read and write object

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -15,15 +15,19 @@ class Test_Base(unittest.TestCase):
     """
     @classmethod
     def setUpClass(self):
+        """
+        Set up the first two instances of objects for use in testing Base class methods.
+        """
         self.B1 = Base()
+        self.R1 = Rectangle(1, 2)
 
     def test_to_json_string(self):
         """ Test 'Base' Static Method via inheritance with Rectangle subclass"""
 
-        R1 = Rectangle(1,3, id=2)
-        l1 = []
-        l1.append(R1.to_dictionary())
-        self.assertEqual(R1.to_json_string(l1), '[{"id": 2, "width": 1, "height": 3, "x": 0, "y": 0}]')
+        R1 = self.R1
+        obj_list = []
+        obj_list.append(R1.to_dictionary())
+        self.assertEqual(R1.to_json_string(obj_list), '[{"id": 2, "width": 1, "height": 2, "x": 0, "y": 0}]')
 
     
 
@@ -37,8 +41,22 @@ class Test_Base(unittest.TestCase):
 
         self.assertEqual(3, base2.id)
 
-    def test_(self):
-        pass
+    def test_obj_save_and_recover(self):
+        """
+        Test save to file method.
+        """
+        Instance = self.R1
+        
+        Instance.save_to_file([Instance])
+        
+        file_name = "Rectangle.json"
+        with open(file_name, "r") as file:
+            read_string = file.read()
+        
+        expected_string = '[{"id": 2, "width": 1, "height": 2, "x": 0, "y": 0}]'
+        self.assertEqual(read_string, expected_string)
+
+        
 
 
 if __name__ == "__main__":
