ac4b59a14ad30e4ddf3cd3f9c037ad8459232b9e
Mark <marknjoropge@gmail.com>
2023-08-15 22:53:36 +0300

    Add tasks 1 and 2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Script creates a database
+-- Create database hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Delete database
+-- Deletes "hbtn_0c_0" database
+DROP DATABASE IF EXISTS hbtn_0c_0;
