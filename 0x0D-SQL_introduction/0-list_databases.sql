8a43dfc8a7174ef6a4df84b4d065fd5cd30ca9f4
Mark <marknjoropge@gmail.com>
2023-08-15 20:54:05 +0300

    Add task 0

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,4 @@
+-- Lists all databases of a MySQL server
+
+-- list all databases of the server
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,3 @@
+## 0x0D. SQL - Introduction
+
+An introduction to databases and databse management systems.
