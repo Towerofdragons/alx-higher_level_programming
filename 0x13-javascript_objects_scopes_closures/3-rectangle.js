4afb98748c805675567c3b10692eb5abdb4b4450
Mark <marknjoropge@gmail.com>
2023-09-12 20:36:17 +0000

    Add README and tasks 0-3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle{
+	constructor(w, h)
+	{
+		if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0)
+		{
+			this.height = h;
+			this.width = w;
+		}
+	}
+
+	print() {
+		for (let i = 0; i < this.height; i++)
+		{
+			let line = "";
+			let y = 0;
+
+			while (y < this.width)
+			{
+				line += "X";
+				y++;
+			}
+			console.log(line);
+		}
+	}
+}
+
+module.exports = Rectangle;
