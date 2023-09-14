319a63ecdb7e485a06473e2e3bcb73e4b6b8fce2
Mark <marknjoropge@gmail.com>
2023-09-14 21:08:42 +0000

    Add tasks 7 and 8

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+let square = require("./5-square.js");
+
+class Square extends square{
+	charPrint(c){
+		let print_char = "";
+		if (c === undefined){
+			print_char = 'X';
+		}
+		else
+		{
+			print_char = c;
+		}
+
+		for(let h = 0; h < this.height; h++){
+			let line = "";
+			for (let w = 0; w < this.width; w++){
+				line += print_char;
+			}
+			console.log(line);
+		}
+	}
+}
+
+module.exports = Square
