354116d64a3cfbbd1d56b4cc2b0b9a91567a0e44
vagrant <vagrant@ubuntu-20.04>
2023-09-17 20:34:34 +0000

    Add task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,10 +8,10 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host ="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", argv[4])
+    cur.execute("SELECT * FROM states WHERE name={} ORDER BY id ASC".format(argv[4]))
 
     states = cur.fetchall()
 
