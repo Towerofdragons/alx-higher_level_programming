caa8fad40b9371ae460f8a4665f258f3c22d5644
vagrant <vagrant@ubuntu-20.04>
2023-09-17 19:45:45 +0000

    Add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"Script lists all states from database 'hbtn_0e_0_usa'"
+
+import MYSQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host ="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
