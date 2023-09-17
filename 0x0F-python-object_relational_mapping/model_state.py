441b4853133193fc6056390ada813893d654199b
vagrant <vagrant@ubuntu-20.04>
2023-09-17 21:44:10 +0000

    Add task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,8 @@ utilize ORM
 
 from model_state import Base, State
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-        eng= create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(eng)
