a130dab0587268a2e1e19eb87b7010aff8106fd0
vagrant <vagrant@ubuntu-20.04>
2023-09-30 09:21:37 +0000

    Added final tasks

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+if (args.length === 0 || args.length === 1) {
+	  console.log(0);
+} else {
+	  argumentList = args.map(Number);
+
+	  arguementList.sort(function (a, b) { return b - a; });
+
+	  const ans = argumentList[1];
+	  console.log(ans);
+}
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+exports.add = function add (a, b) 
+		{
+			return Number(a + b);
+		};
