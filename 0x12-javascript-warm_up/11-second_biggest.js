35c5053324d42e3e06cff33369c47a179e330021
vagrant <vagrant@ubuntu-20.04>
2023-09-30 09:35:20 +0000

    Added final tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 let args = process.argv.slice(2);
-if (args.length === 0 || args.length === 1) {
+if (args.length === 0 || args.length === 1) 
+{
 	  console.log(0);
-} else {
-	  argumentList = args.map(Number);
-
-	  arguementList.sort(function (a, b) { return b - a; });
-
-	  const ans = argumentList[1];
-	  console.log(ans);
+} 
+else 
+{
+	  
+	argumentList = args.map(Number);
+	arguementList.sort(function (a, b) { return b - a; });
+	const ans = argumentList[1];
+	console.log(ans);
 }
