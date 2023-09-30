d28c078ccc7f5b489dd771d2832368e2f025a0e8
vagrant <vagrant@ubuntu-20.04>
2023-09-30 01:35:55 +0000

    Add README and task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Fetch https://alx-intranet.hbtn.io/status using urllib
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        res =  response.read()
+        print("Body response:")
+        print(f"\t- type: {type(res)}\n\t- content:{res}\n\t- utf8 content: {res.decode('utf-8')}")
