d857134b2725d65989d8db0172c5a3093f6f69a0
vagrant <vagrant@ubuntu-20.04>
2023-09-30 02:54:29 +0000

    Add task 7 untested, running

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ takes in a URL, sends a request to the URL and displays the body of the response
 import sys
 import requests
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     res = requests.get(sys.argv[1])
     code = res.status_code
     if code >= 400:
