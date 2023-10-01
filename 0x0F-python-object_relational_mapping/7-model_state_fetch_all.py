dfa31056a2b7dd9c4f4495e0574d172e1eb80e63
vagrant <vagrant@ubuntu-20.04>
2023-10-01 16:26:33 +0000

    Add task 7 documentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Lists all state objects from the db `hbtn_0e_6_usa`.
+Lists all state objects from 
+the db `hbtn_0e_6_usa`.
 """
-
 
 from sys import argv
 from model_state import State
 
-from model_state importBase
+from model_state import Base
 from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker
@@ -15,7 +15,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Get all states from DB
+    Get all states from the DB
+    After accessing db.
     """
     db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
 
