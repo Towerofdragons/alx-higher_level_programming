94fe0013ad7eb92983a91ccf63eb966cfd8603e6
vagrant <vagrant@ubuntu-20.04>
2023-10-01 16:18:21 +0000

    Add task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists all state objects from the db `hbtn_0e_6_usa`.
+"""
+
+
+from sys import argv
+from model_state import State
+
+from model_state importBase
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+
+    engine = create_engine(db)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for inst in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(inst.id, inst.name))
