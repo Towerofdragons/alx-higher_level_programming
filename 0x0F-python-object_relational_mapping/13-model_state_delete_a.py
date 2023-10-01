f4f7db0a06b5f3684fc39c59f88da760d7385569
vagrant <vagrant@ubuntu-20.04>
2023-10-01 17:12:40 +0000

    Add task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Delete all State objects with a name containing the letter `a`.
+"""
+
+from sys import argv
+
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+        db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+
+        engine = create_engine(db)
+        session = sessionmaker(bind=engine)()
+        states = session.query(State).filter(State.name.contains('a'))
+        if states is not None:
+            for state in states:
+                session.delete(state)
+                session.commit()
+                session.close()
